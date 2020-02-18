1b8563bf1aca8d58cbeafd959a351649ea31cb94
kaczor6418 <andrzej6418@gmail.com>
2020-02-19 00:09:03 +0100

    feat(main): finish sorting if no swap

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,21 @@ import {swapTwoElementsOfArray} from "./Utils";
 function bubbleSort(elementsList: number[], direction: SortDirection = SortDirection.ASC): void {
     const totalListLength: number = elementsList.length;
     const comparator: (comparedElement: number, selectedElement: number) => boolean = direction === SortDirection.ASC ? ascendingCondition : descendingCondition;
+    let isSwapped: boolean = false;
+    let i: number = 0;
     let finish: number = 0;
     while (finish < totalListLength - 1) {
-        let i: number = 0;
+        isSwapped = false;
+        i = 0;
         for (let j = i + 1; j < totalListLength - finish; j++) {
             if (comparator(elementsList[i], elementsList[j])) {
                 swapTwoElementsOfArray(elementsList, i, j);
+                isSwapped = true;
             }
             i++;
+        }
+        if (!isSwapped) {
+            break;
         }
         finish++;
     }
